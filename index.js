79b2401bf94cc3f4b8781fae91d6b21515e97b6b
tomasvaro <tomas.varo63@gmail.com>
2019-05-17 16:31:45 +0200

    Fixed bottom element disappear mm

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 (function startup() {
     const newTodoForm = document.querySelector("#new-todo-form");
     const textbox = newTodoForm.querySelector("#new-todo");
+    const section = document.querySelector("section");
+    const checkAll = document.querySelector("#check-all");
 
     newTodoForm.onsubmit = event => event.preventDefault();
     newTodoForm.addEventListener("keydown", (event) => {
@@ -10,6 +12,8 @@
             if (textbox.value != "") {
                 createNewTodo(textbox.value);
                 newTodoForm.reset();
+                section.classList.remove("hidden");
+                checkAll.classList.remove("hidden");
             }
         }
     });
@@ -18,51 +22,16 @@
     const checkAllButton = document.querySelector("#check-all");
     checkAllButton.addEventListener("mousedown", () => {
         onCheckAllButtonClick();
+        updateNrLeft();    
     });
 
-    // Clears all completed
+    // Removes Todo-item
     const checkClearButton = document.querySelector("#clear-button");
     checkClearButton.addEventListener("mousedown", () => {
         onClearButtonClick();
+        updateNrLeft();
     })
 })();
-
-function onCheckAllButtonClick(){
-    const todoItems = Array.from(document.querySelectorAll(".todo-item:not(.todo-item-blueprint)"));
-    const allChecked = todoItems.every(ti => ti.querySelector(".checkbox-round input").checked === true);
-
-    if (allChecked === true) {
-        todoItems.forEach(ti => {
-            ti.querySelector(".checkbox-round input").checked = false;
-            updateCheckboxStyle(ti);
-        });
-    } else {
-        todoItems.forEach(ti => {
-            ti.querySelector(".checkbox-round input").checked = true;
-            updateCheckboxStyle(ti);
-        });
-    }
-}
-
-function onClearButtonClick(){
-    const todoItemsChecked = Array.from(document.querySelectorAll(".todo-item:not(.todo-item-blueprint)"))
-        .filter(ti => ti.querySelector(".checkbox-round input").checked === true);
-    
-    todoItemsChecked.forEach(ti => ti.remove());
-}
-
-function updateCheckboxStyle(listItem) {
-    // Get child elements.
-    const checkbox = listItem.querySelector(".checkbox-round");
-
-    if (checkbox.querySelector("input").checked) {
-        checkbox.classList.add("checked");
-        listItem.querySelector(".todo-label").classList.add("checked");
-    } else {
-        checkbox.classList.remove("checked");
-        listItem.querySelector(".todo-label").classList.remove("checked");
-    }
-}
 
 // Creates a new todo list item element.
 function createNewTodo(text) {
@@ -77,7 +46,10 @@ function createNewTodo(text) {
 
     // Remove self on click.
     createdListItem.querySelector(".todo-button-remove")
-        .addEventListener("click", () => { createdListItem.remove(); });
+        .addEventListener("click", () => { 
+            createdListItem.remove();
+            ifToDolistEmpty();
+        });
 
     const textbox = createdListItem.querySelector(".todo-textbox");
     const label = createdListItem.querySelector(".todo-label");
@@ -115,6 +87,81 @@ function createNewTodo(text) {
 
     const checkbox = checkboxRound.querySelector("input");
     checkbox.addEventListener("change", () => { updateCheckboxStyle(createdListItem); });
+    updateNrLeft();
+}
+
+// Checks/unchecks all items in Todo-list
+function onCheckAllButtonClick(){
+    const todoItems = Array.from(document.querySelectorAll(".todo-item:not(.todo-item-blueprint)"));
+    const allChecked = todoItems.every(ti => ti.querySelector(".checkbox-round input").checked === true);
+
+    if (allChecked === true) {
+        todoItems.forEach(ti => {
+            ti.querySelector(".checkbox-round input").checked = false;
+            updateCheckboxStyle(ti);
+        });
+    } else {
+        todoItems.forEach(ti => {
+            ti.querySelector(".checkbox-round input").checked = true;
+            updateCheckboxStyle(ti);
+        });
+    }
+    updateNrLeft();
+}
+
+// Removes all checked Todo-items
+function onClearButtonClick(){
+    const todoItemsChecked = Array.from(document.querySelectorAll(".todo-item:not(.todo-item-blueprint)"))
+        .filter(ti => ti.querySelector(".checkbox-round input").checked === true);
+    todoItemsChecked.forEach(ti => ti.remove());
+    ifToDolistEmpty();
+    updateNrLeft();
+}
+
+// Removes bottom section and "check-all button".
+function ifToDolistEmpty(){
+    const todoItems = Array.from(document.querySelectorAll(".todo-item:not(.todo-item-blueprint)"));
+    // const todoItemsChecked = Array.from(document.querySelectorAll(".todo-item:not(.todo-item-blueprint)"))
+    //     .filter(ti => ti.querySelector(".checkbox-round input").checked === true);
+    const section = document.querySelector("section");
+    const checkAll = document.querySelector("#check-all");
+
+    if(todoItems.length === 0) {
+        section.classList.add("hidden");
+        checkAll.classList.add("hidden");
+    }
+}
+
+// Updates number of unchecked Todo-items.
+function updateNrLeft(){
+    const todoItemsChecked = Array.from(document.querySelectorAll(".todo-item:not(.todo-item-blueprint)"))
+        .filter(ti => ti.querySelector(".checkbox-round input").checked === false);
+    const nrLeft = document.querySelector("#nr-left");
+    const itemsLeft = document.querySelector("#items-left");
+
+    nrLeft.textContent = todoItemsChecked.length;
+    if(todoItemsChecked.length === 0 || todoItemsChecked.length > 1){
+        itemsLeft.textContent = " items left";
+    }
+    else{
+        itemsLeft.textContent = " item left";
+    }
+}
+
+
+
+
+function updateCheckboxStyle(listItem) {
+    // Get child elements.
+    const checkbox = listItem.querySelector(".checkbox-round");
+
+    if (checkbox.querySelector("input").checked) {
+        checkbox.classList.add("checked");
+        listItem.querySelector(".todo-label").classList.add("checked");
+    } else {
+        checkbox.classList.remove("checked");
+        listItem.querySelector(".todo-label").classList.remove("checked");
+    }
 }
 
 
